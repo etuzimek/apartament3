d8bfc40ac7720f05fd4c5591916500a24fe9edf4
qazwaz <edwardtuzimek@gmail.com>
2023-04-06 19:42:49 +0200

    App pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Apartment from "./components/pages/Apartment";
+import Gallery from "./components/pages/Gallery";
+import Rules from "./components/pages/Rules";
+import Contact from "./components/pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Apartment} />
+        <Route path="/contact" component={Gallery} />
+        <Route path="/contact" component={Rules} />
+        <Route path="/contact" component={Contact} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

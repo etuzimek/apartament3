d304b2f2c416e1b46d1267cd85e3c8564013c25a
qazwaz <edwardtuzimek@gmail.com>
2023-04-07 10:17:10 +0200

    Apartment

--- src/components/pages/Apartment.js
+++ src/components/pages/Apartment.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Apartment.css";
 
-function Apartment() {
+export default function Apartment() {
   return (
-    <div>Apartment</div>
-  )
+    <>
+      <div className="apartment">
+        <h1>APARTAMENT</h1>
+        <p>
+          o wysokim standardzie w sklad ktorego wchodza dwie sypialnie, salon z
+          aneksem kuchennym, lazienka i balkon.
+        </p>
+        <div className="apartment-wrapper">
+          <img src="../images/apartament1.jpg" alt="apartament" />
+          <div className="box" id="obrazek1">
+            <img src="../images/apartament1.jpg" alt="apartament" />
+          </div>
+          <img src="../images/apartament2.jpg" alt="apartament" />
+          <div className="box" id="obrazek2">
+            <img src="../images/apartament2.jpg" alt="apartament" />
+          </div>
+          <img src="../images/apartament3.jpg" alt="apartament" />
+          <div className="box" id="obrazek3">
+            <img src="../images/apartament3.jpg" alt="apartament" />
+          </div>
+          <img src="../images/apartament4.jpg" alt="apartament" />
+          <div className="box" id="obrazek1">
+            <img src="../images/apartament4.jpg" alt="apartament" />
+          </div>
+          <img src="../images/apartament5.jpg" alt="apartament" />
+          <div className="box" id="obrazek1">
+            <img src="../images/apartament5.jpg" alt="apartament" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default Apartment
